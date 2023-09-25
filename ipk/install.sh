50626018ec13434dafc75226a923c6ec94f44948
‘Zeleza’ <mail@zeleza.ru>
2023-09-25 17:34:21 +0300

    Доработан код функции по добавлению гостевых сетей (kvas vpn guest add all).

--- ipk/install.sh
+++ ipk/install.sh
@@ -6,9 +6,9 @@ NOCL="\033[m";
 LENGTH=68
 print_line()(printf "%83s\n" | tr " " "-")
 diff_len() {
-	charlen=$(echo "${1}" | sed -r "s/[\]033\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g")
-	charlen=${#charlen}
-	echo $(( LENGTH - charlen ))
+        charlen=$(echo "${1}" | sed -r "s/[\]033\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g")
+        charlen=${#charlen}
+        echo $(( LENGTH - charlen ))
 }
 ready() {
         size=$(diff_len "${1}")
@@ -21,35 +21,36 @@ package_name=kvas_all.ipk
 package_url=https://github.com/qzeleza/kvas/releases/latest/download/${package_name}
 list_backup=/opt/etc/hosts.list.backup
 
-
+clear
 print_line
-ready "${GREEN}Начинаем установку пакета КВАС${NOCL}"
-echo
-mkdir -p /opt/packages
-cd /opt/packages || {
-        echo "Невозможно создать папку /opt/packages";
-        exit 1
+echo -e "${GREEN}Начинаем установку пакета КВАС${NOCL}"
+
+mkdir -p /opt/packages || {
+	echo "Невозможно создать папку /opt/packages";
+	exit 1
 }
 print_line
-ready 'Обновляем opkg...            '
+ready 'Обновляем opkg...'
 {
-        opkg update && opkg upgrade && opkg install curl iptables
+	opkg update && opkg upgrade && opkg install curl iptables
 } &>/dev/null && when_ok || when_err
 
-ready 'Загружаем пакет...            '
+ready 'Загружаем пакет...'
 {
-        [ -f /opt/bin/kvas ] && kvas export ${list_backup}
-        curl -L "${package_url}" -o  ${package_name}
+	[ -f /opt/bin/kvas ] && kvas export "${list_backup}"
+	cd /opt/packages
+	curl -sOL "${package_url}"
 } &>/dev/null && when_ok || when_err
 
-ready 'Устанавливаем пакет...            '
+ready 'Устанавливаем пакет...'
 {
-        opkg install "./${package_name}"
+	opkg install "./${package_name}"
 } &>/dev/null && when_ok || when_err
+
 print_line
 sleep 2
-{
-	    clear
-        kvas setup
-} && rm -f ./install.sh
 
+{
+    clear
+    kvas setup
+} && rm -f ./install.sh
