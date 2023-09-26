7bb8de753d7d3c06ec41e4121ab904f3b8bcedac
‘Zeleza’ <mail@zeleza.ru>
2023-09-26 11:53:56 +0300

    Добавлен способ обновления пакета на новую версию путем установки скрипта, который удаляет старый пакет и восстанавливает список разблокировки.

--- ipk/update.sh
+++ ipk/update.sh
@@ -20,6 +20,7 @@ when_err() (echo -e "${RED}ОШИБКА${NOCL}" )
 package_name=kvas_all.ipk
 package_url=https://github.com/qzeleza/kvas/releases/latest/download/${package_name}
 list_backup=/opt/etc/hosts.list.backup
+host_list=/opt/etc/hosts.list
 
 clear
 print_line
@@ -35,22 +36,44 @@ ready 'Обновляем opkg...'
 	opkg update && opkg upgrade && opkg install curl iptables
 } &>/dev/null && when_ok || when_err
 
+
 ready 'Загружаем пакет...'
 {
-	[ -f /opt/bin/kvas ] && kvas export "${list_backup}"
+
 	cd /opt/packages
 	curl -sOL "${package_url}"
 } &>/dev/null && when_ok || when_err
 
-ready 'Устанавливаем пакет...'
+[ -f "${host_list}" ] && {
+	ready 'Сохраняем список разблокировки в архив...'
+	mv "${host_list}" "${list_backup}" &>/dev/null && when_ok || when_err
+}
+
+ready 'Удаляем предыдущую версию пакета...'
+{
+	kvas rm yes
+
+} &>/dev/null && when_ok || when_err
+
+ready 'Устанавливаем новую версию пакета...'
 {
 	opkg install "./${package_name}"
+
 } &>/dev/null && when_ok || when_err
 
 print_line
-sleep 2
+sleep 1
 
 {
     clear
-    kvas setup
-} && rm -f ./install.sh
+    kvas setup update
+}
+
+[ -f "${list_backup}" ] && {
+	ready 'Восстанавливаем список разблокировки из архива...'
+	kvas import "${list_backup}" &>/dev/null && when_ok || when_err
+}
+
+print_line
+sleep 2
+rm -f ./update.sh
